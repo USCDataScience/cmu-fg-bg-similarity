c7c2da863a655e6e0ca3f193f8d97a7aae2f9880
Chris Mattmann <chris.a.mattmann@jpl.nasa.gov>
2020-04-18 21:26:52 -0700

    Fix port to 8001.

--- scripts/file_similarity-fg.sh
+++ scripts/file_similarity-fg.sh
@@ -1,2 +1,2 @@
 export FILENAME="$1"
-curl -X POST http://localhost:8888/CounterfeitElectrics_foreImg -H "content-type:text" -d "http://localhost:8000/${FILENAME}"
+curl -X POST http://localhost:8888/CounterfeitElectrics_foreImg -H "content-type:text" -d "http://localhost:8001/${FILENAME}"
